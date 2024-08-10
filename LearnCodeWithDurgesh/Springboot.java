a416d12f588f78237c8f8c301dda7e987090d9d6
Pratik Sharma <118264213+pratiksh21@users.noreply.github.com>
2024-08-10 07:30:03 +0530

    Create Springboot.java

---
+++ LearnCodeWithDurgesh/Springboot.java
@@ -0,0 +1,8 @@
+Q1  what is springboot?
+  1.Springboot is a java framework that makes it easier to create and run java application.
+  2. It simplifies the configuration and setup process,allowing developers to focus more on writing code for their applications.
+  3. Springboot facilates rapid application development that allows developers to get production ready code.
+
+Q2. What is maven central repository ?
+  The Maven Central Repository is a public repository that Java developers can use to find open-source libraries and artifacts for Maven projects.
+  
